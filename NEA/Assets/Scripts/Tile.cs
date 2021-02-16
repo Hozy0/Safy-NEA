72155bb1aa0ec48590d8fbd7fde1bba02ee32ad4
Hozy0 <79100897+Hozy0@users.noreply.github.com>
2021-02-16 18:45:07 +0000

    Hover Effect
    I updated the tiles.cs script to create an hover effect that slightly zooms the tile to show it is "selected"

--- NEA/Assets/Scripts/Tile.cs
+++ NEA/Assets/Scripts/Tile.cs
@@ -7,14 +7,29 @@ public class Tile : MonoBehaviour
     private SpriteRenderer rend;
     public Sprite[] tileGraphics;
 
+    public float hoverAmount;
+
     private void Start()
         {
 
-        rend = GetComponent<SpriteRenderer>();
-        int randTile = Random.Range(0, tileGraphics.Length);
-        rend.sprite = tileGraphics[randTile];
+            rend = GetComponent<SpriteRenderer>();
+            int randTile = Random.Range(0, tileGraphics.Length);
+            rend.sprite = tileGraphics[randTile];
 
         }
+
+
+    private void OnMouseEnter()
+        {
+
+            transform.localScale += Vector3.one * hoverAmount;
+         }
+
+    private void OnMouseExit()
+    {
+
+        transform.localScale -= Vector3.one * hoverAmount;
+    }
 
 
 }
