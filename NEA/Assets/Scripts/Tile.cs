0d9f156711262b3129e16f246b46f9e523062de7
Hozy0 <79100897+Hozy0@users.noreply.github.com>
2021-02-16 15:53:51 +0000

    Setting up the battlefield
    I've jus set up a battlefield using the tile prefab. A prefab is similar to a class as we can create a lot of duplicate objects with the same attributes and scripts.
    I've also created the tile.cs script to randomize the tile appearance. I'm hoping to use this funtionality in future where tiles that look differently will have different moving costs

---
+++ NEA/Assets/Scripts/Tile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private SpriteRenderer rend;
+    public Sprite[] tileGraphics;
+
+    private void Start()
+        {
+
+        rend = GetComponent<SpriteRenderer>();
+        int randTile = Random.Range(0, tileGraphics.Length);
+        rend.sprite = tileGraphics[randTile];
+
+        }
+
+
+}
