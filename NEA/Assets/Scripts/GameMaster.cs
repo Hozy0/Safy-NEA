86c25f70ffd4ead9ca501791b4c4134ad972cbf5
Hozy0 <79100897+Hozy0@users.noreply.github.com>
2021-02-18 13:24:09 +0000

    Movement 2
    Added a move function that allows me to move the unit on the grid.
    I wanted my unit to be able to move in one direction at the time so I made the  movement in the X axis separate fromthe one in the Y axis

--- NEA/Assets/Scripts/GameMaster.cs
+++ NEA/Assets/Scripts/GameMaster.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class GameMaster : MonoBehaviour
 {
     public Units selectedUnit;
+
+    public void ResetTiles()
+    {
+        foreach (Tile tile in FindObjectsOfType<Tile>()) //this allows me to create a function resets the tiles to their original colour
+        {
+            tile.Reset();
+        }
+    }
 }
--- NEA/Assets/Scripts/Units.cs
+++ NEA/Assets/Scripts/Units.cs
@@ -9,7 +9,9 @@ public class Units : MonoBehaviour
 
     public int movementRange;
     public bool hasMoved;
-    
+
+    public float moveSpeed;
+
     private void Start()
     {
         //this allows the script to access all public attributes and methode from GameMaster script
@@ -23,6 +25,7 @@ public class Units : MonoBehaviour
         {
             selected = false;
             gm.selectedUnit = null; //because there isn't a selected unit anymore
+            gm.ResetTiles();
         } else
         {
             if(gm.selectedUnit != null)
@@ -32,6 +35,8 @@ public class Units : MonoBehaviour
             //this now allows me to select the unit the player clicking on
             selected = true;
             gm.selectedUnit = this; //this refers to the this instance of the Units script thats attached to the character the player is clicking
+
+            gm.ResetTiles();
             GetWalkableTiles();
         }
 
@@ -57,4 +62,27 @@ public class Units : MonoBehaviour
         }
     }
 
+
+    public void Move(Vector2 tilePos)
+    {
+        gm.ResetTiles();
+        StartCoroutine(StartMovement(tilePos));
+    }
+
+    IEnumerator StartMovement(Vector2 tilePos)
+    {
+        while(transform.position.x != tilePos.x)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(tilePos.x, transform.position.y), moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        while (transform.position.y != tilePos.y)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, tilePos.y), moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        hasMoved = true;
+    }
 }
